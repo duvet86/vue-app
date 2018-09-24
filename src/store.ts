1fd9237d7d45e8a7e46a8053017d12c385461a9d
Luca <duvet86@gmail.com>
2018-09-24 12:04:09 +0800

    Working.

--- src/store.ts
+++ src/store.ts
@@ -3,8 +3,35 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+export interface IPerson {
+  first_name: string;
+  last_name: string;
+}
+
+interface IState {
+  people: IPerson[];
+}
+
+export default new Vuex.Store<IState>({
+  state: {
+    people: []
+  },
+  mutations: {
+    setPeople(state, people: IPerson[]) {
+      state.people = people;
+    }
+  },
+  actions: {
+    async getPeople({ commit }) {
+      try {
+        const data = await fetch("/api/data");
+        const people: IPerson[] = await data.json();
+
+        commit("setPeople", people.slice(0, 10));
+      } catch (e) {
+        // tslint:disable-next-line:no-console
+        console.error(e);
+      }
+    }
+  }
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,19 +3,42 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Diego e' il migliore"/>
     Message: {{message}}
+    <div class="cards">
+      <Card v-for="person in people" :key="person.id" :person="person"></Card>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import store, { IPerson } from "@/store";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Card from "@/components/Card.vue";
 
 @Component({
   components: {
-    HelloWorld
+    HelloWorld,
+    Card
   }
 })
 export default class Home extends Vue {
-  public message = "Hello";
+  private message = "Hello";
+
+  // Computed property.
+  get people() {
+    return store.state.people;
+  }
+
+  // Life cycle hook.
+  public mounted() {
+    store.dispatch("getPeople");
+  }
 }
 </script>
+
+<style scoped>
+.cards {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
